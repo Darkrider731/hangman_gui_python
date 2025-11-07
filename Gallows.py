41503080ce44fa24a15dd059c84669d376a82058
Darkrider731 <Katzrefael5@gmail.com>
2025-11-07 05:14:22 +0200

    Implement gallows drawing functions in Pygame
    Added functions to draw various stages of a gallows in Pygame.

---
+++ Gallows.py
@@ -0,0 +1,81 @@
+import pygame as pg
+
+
+def gallows_1(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    
+def gallows_2(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    
+def gallows_3(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 100), (180, 100), 5)
+
+
+def gallows_4(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 100), (180, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (180, 100), (180, 125), 5)
+
+
+
+def gallows_5(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 100), (180, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (180, 100), (180, 125), 5)
+    pg.draw.circle(screen, (180, 200, 31), (180, 142), 12, 3)
+
+
+def gallows_6(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 100), (180, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (180, 100), (180, 125), 5)
+    pg.draw.circle(screen, (180, 200, 31), (180, 142), 12, 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 155), (180, 185), 3)
+
+def gallows_7(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 100), (180, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (180, 100), (180, 125), 5)
+    pg.draw.circle(screen, (180, 200, 31), (180, 142), 12, 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 155), (180, 185), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 157), (190, 170), 3)
+    
+def gallows_8(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 100), (180, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (180, 100), (180, 125), 5)
+    pg.draw.circle(screen, (180, 200, 31), (180, 142), 12, 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 155), (180, 185), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 157), (190, 170), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 157), (170, 170), 3)
+
+def gallows_9(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 100), (180, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (180, 100), (180, 125), 5)
+    pg.draw.circle(screen, (180, 200, 31), (180, 142), 12, 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 155), (180, 185), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 157), (190, 170), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 157), (170, 170), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 185), (190, 198), 3)
+    
+def gallows_10(screen):
+    pg.draw.line(screen, (125, 200, 31), (230, 240), (280, 240), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 240), (255, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (255, 100), (180, 100), 5)
+    pg.draw.line(screen, (125, 200, 31), (180, 100), (180, 125), 5)
+    pg.draw.circle(screen, (180, 200, 31), (180, 142), 12, 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 155), (180, 185), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 157), (190, 170), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 157), (170, 170), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 185), (190, 198), 3)
+    pg.draw.line(screen, (180, 200, 31), (180, 185), (170, 198), 3)
